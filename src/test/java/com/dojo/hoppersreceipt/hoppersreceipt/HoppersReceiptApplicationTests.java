c2637d17698ec55ff99ae64d0dc5d020f1e2e6f4
Marsi <agalliumars@gmail.com>
2023-01-17 14:43:11 +0100

    Initial commit

---
+++ src/test/java/com/dojo/hoppersreceipt/hoppersreceipt/HoppersReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.hoppersreceipt.hoppersreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersReceiptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
